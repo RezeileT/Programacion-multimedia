57bc6fdc3af013cae0f53f0ffa9537167e3f6dc0
AlumnoAfternoon <sebastianbarrios1812@gmail.com>
2025-11-19 20:38:17 +0100

    19/11/25 - Apuntes y sintaxis

---
+++ src/Ejecicio4.kt
@@ -0,0 +1,28 @@
+fun main(){
+    val numeros = listOf(1,2,3,4,5,6,7,8,9,10)
+
+    //Números pares
+    val pares = numeros.filter{it % 2 == 0 }
+    println("Números pares: $pares")
+
+    //Números pares multiplicados por dos
+    val paresDobles = numeros.filter{it % 2 == 0}.map{it * 2 }
+    println("Pares duplicados: $paresDobles")
+
+    //Suma de números mayores a 5
+    val sumasGrandes = numeros.filter { it > 5 }.sum()
+    println("Suma de números mayores a 5: $sumasGrandes")
+
+    //Verificar si hay números primos
+    fun esPrimo(numero: Int): Boolean{
+        if(numero <= 1) return false
+        for (i in 2 until numero){
+            if(numero % i == 0){
+                return false
+            }
+        }
+        return true
+    }
+    val primos = numeros.filter { esPrimo(it) }
+    println("Primos: $primos")
+}
---
+++ src/Ejercicio5.kt
@@ -0,0 +1,58 @@
+//Creación del objeto Usuario
+data class Usuario(val nombre: String, val email: String, val edad: Int)
+
+//Función que valida el nombre del Usuario
+fun validarNombre(nombre: String): Result<String> {
+    return if(nombre.length < 3){
+        Result.failure(Exception("El nombre debe de tener al menos 3 caracteres"))
+    }else {
+        Result.success(nombre)
+    }
+}
+
+fun validarEmail(email: String): Result<String> {
+    return if(email.contains("@") && email.contains(".")) {
+        Result.success(email)
+    }else {
+        Result.failure(Exception("El email no es valido"))
+    }
+}
+
+fun validarEdad(edad: Int): Result<String> {
+    return if(edad in 18..120) {
+        Result.success(edad.toString())
+    }else {
+        Result.failure(Exception("La edad no es valida, tiene que ser entre 18 y 120"))
+    }
+}
+
+//Ejecución del código
+fun main(){
+    //Petición al usuario para que se registre
+    println("Nombre: ")
+    val nombre = readLine() ?: ""
+
+    println("Email: ")
+    val email = readLine() ?: ""
+
+    println("Edad: ")
+    val edad = readLine()?.toIntOrNull() ?: -1
+
+    //Validación de campos
+    val resulatadoNombre = validarNombre(nombre)
+    val resultadoEmail = validarEmail(email)
+    val resultadoEdad = validarEdad(edad)
+
+    //Devolución de resultados y errores
+    when {
+        resulatadoNombre.isFailure -> println("Error en el nombre: ${resulatadoNombre.exceptionOrNull()?.message}")
+        resultadoEmail.isFailure -> println("Error en el email: ${resultadoEmail.exceptionOrNull()?.message}")
+        resultadoEdad.isFailure -> println("Error en la edad: ${resultadoEdad.exceptionOrNull()?.message}")
+        else -> {
+            //Inicializamos el usuario tras hacer las verificaciones
+            val usuario = Usuario(nombre, email, edad)
+            println("Usuario credado: ${usuario.nombre}")
+        }
+    }
+
+}
