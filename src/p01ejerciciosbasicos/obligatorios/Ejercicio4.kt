dd69b6de4462d64a63bd2b427aafef6cbd84257b
AlumnoAfternoon <sebastianbarrios1812@gmail.com>
2025-11-26 18:21:00 +0100

    26/11/25 - Completado del cuarto ejercicio obligatorio de la primera práctica

--- src/p01ejerciciosbasicos/obligatorios/Ejercicio4.kt
+++ src/p01ejerciciosbasicos/obligatorios/Ejercicio4.kt
@@ -1,9 +1,8 @@
 package p01ejerciciosbasicos.obligatorios
 
-import java.util.Locale
 import java.util.Locale.getDefault
 
-data class Contactos(var nombre:String, var email:String, var telefono:String, var favorito: Boolean){
+data class Contacto(var nombre:String, var email:String, var telefono:String, var favorito: Boolean){
     override fun toString(): String {
         return "Nombre: $nombre\n" +
                 "Email: $email\n" +
@@ -11,12 +10,12 @@ data class Contactos(var nombre:String, var email:String, var telefono:String, v
     }
 }
 
-var contactos = mutableListOf<Contactos>()
+var contactos = mutableListOf<Contacto>()
 
 fun main(){
-    contactos.add(Contactos("Maria","maria@gmail.com","123456789",true))
-    contactos.add(Contactos("Bob","bob@gmail.com","9876584321",false))
-    contactos.add(Contactos("Pedro","pedro@gmail.com","789456123",false))
+    contactos.add(Contacto("Maria","maria@gmail.com","123456789",true))
+    contactos.add(Contacto("Bob","bob@gmail.com","9876584321",false))
+    contactos.add(Contacto("Pedro","pedro@gmail.com","789456123",false))
 
     menuInteractivo()
 }
@@ -27,7 +26,7 @@ fun menuInteractivo(){
         println("\n*** Contactos ***")
         println("1. Crear contacto")
         println("2. Buscar por nombre")
-        println("3. Lista de contactos ordenados")
+        println("3. Lista de contactos (ordenados por nombre)")
         println("4. Lista de favoritos")
         println("5. Marcar como favorito")
         println("6. Eliminar contacto")
@@ -39,10 +38,10 @@ fun menuInteractivo(){
             1 -> crearContacto()
             2 -> buscarPorNombre()
             3 -> obtenerOrdenados()
-            4 -> ""
-            5 -> ""
-            6 -> ""
-            7 -> ""
+            4 -> obtenerFavoritos()
+            5 -> marcarFavorito()
+            6 -> eliminarContacto()
+            7 -> crearContacto()
             8 -> break
             else -> println("Opción inválida")
         }
@@ -77,28 +76,19 @@ fun validarTelefono(telefono: String): Result<String>{
         return Result.failure(Exception("El telefono no puede tener caracteres que no sean numéricos"))
     }
     if (telefono.length !in 9..15){
-        return Result.failure(Exception("El telefono debe tener entre 9 y 15 números"))
+        return Result.failure(Exception("El telefono debe tener entre 9 y 15 digitos"))
     }
     return Result.success(telefono)
 }
 
-fun crearContacto(){
-    println("\n*** Crear contato ***")
-    println("Introduce un nombre: ")
-    val nombre = validarNombre(readlnOrNull().toString())
-    println("Introduce el emal: ")
-    val email = validarEmail(readlnOrNull().toString())
-    println("Introduce el telefono: ")
-    val telefono = validarTelefono(readlnOrNull().toString())
-
-    contactos.add(Contactos(nombre.toString(), email.toString(), telefono.toString(), false))
-}
-
-fun buscarPorNombre(){
+fun buscarPorNombre(): List<Contacto>?{
     println("\n*** Buscar por nombre ***")
     print("Introduce un nombre para filtrar: ")
     val nombre = readlnOrNull().toString()
-    validarNombre(nombre)
+    if (validarNombre(nombre).isFailure){
+        println(validarNombre(nombre).exceptionOrNull()?.message)
+        return null
+    }
     println("Contactos con el nombre: $nombre")
     val contactosFiltrados = contactos.filter{ it.nombre.lowercase(getDefault()).contains(nombre.lowercase(getDefault()))}
     if (contactosFiltrados.isEmpty()) {
@@ -107,13 +97,73 @@ fun buscarPorNombre(){
         println("-------------")
         contactosFiltrados.forEach { println(it) }
     }
+    return contactosFiltrados
 }
 
 fun obtenerOrdenados(){
-    println("\n*** Obtener ordenados ***")
+    println("\n*** Lista de contactos ***")
     contactos.sortedBy { it.nombre }.forEach {
         println("-------------")
         println(it)
     }
 }
 
+fun obtenerFavoritos(){
+    println("\n*** Obtener favoritos ***")
+    contactos.filter{ it.favorito }.forEach {
+        println("-------------")
+        println(it)
+    }
+}
+
+fun marcarFavorito(){
+    println("\n*** Marcar favoritos ***")
+    obtenerOrdenados()
+    println("Introduce el correo del contacto que quieras pasar a favorito: ")
+    val email = readlnOrNull().toString()
+    if (validarEmail(email).isFailure){
+        println(validarEmail(email).exceptionOrNull()?.message)
+        return
+    }
+    contactos.filter { it.email.lowercase(getDefault()).contains(email.lowercase(getDefault())) }.forEach {
+        it.favorito = true;
+    }
+}
+
+fun eliminarContacto(){
+    println("\n*** Eliminar contato ***")
+    obtenerOrdenados()
+    println("\nIntroduce el correo del contacto que quieras eliminar: ")
+    val email = readlnOrNull().toString()
+    if (validarEmail(email).isFailure){
+        println(validarEmail(email).exceptionOrNull()?.message)
+        return
+    }
+    contactos.filter { it.email.lowercase(getDefault()).contains(email.lowercase(getDefault())) }.forEach {
+        contactos.remove(it)
+    }
+}
+
+fun crearContacto(){
+    println("\n*** Crear contato ***")
+    print("Introduce un nombre: ")
+    val nombre = readlnOrNull().toString()
+    if (validarNombre(nombre).isFailure){
+        println(validarNombre(nombre).exceptionOrNull()?.message)
+        return
+    }
+    print("Introduce el correo del contacto: ")
+    val email = readlnOrNull().toString()
+    if (validarEmail(email).isFailure){
+        println(validarEmail(email).exceptionOrNull()?.message)
+        return
+    }
+    print("Introduce el telefono: ")
+    val telefono = readlnOrNull().toString()
+    if (validarTelefono(telefono).isFailure){
+        println(validarTelefono(telefono).exceptionOrNull()?.message)
+        return
+    }
+    contactos.add(Contacto(nombre, email, telefono, false))
+    println("Contacto añadido")
+}
